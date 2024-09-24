34f1ad5a8e5c355530228626b176f6551e5ba34d
wejdene <wejdene.haouari1@gmail.com>
2024-09-24 00:39:05 -0400

    added index.htm;

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,59 @@ var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var open = require('gulp-open');
 
+// Define paths
 var Paths = {
   HERE: './',
   DIST: 'dist/',
   CSS: './assets/css/',
   SCSS_TOOLKIT_SOURCES: './assets/scss/soft-design-system.scss',
-  SCSS: './assets/scss/**/**'
+  SCSS: './assets/scss/**/**',
+  HTML: './**/*.html',
+  IMAGES: './assets/img/**/*'
 };
 
-gulp.task('compile-scss', function() {
+// Task to compile SCSS
+gulp.task('compile-scss', function () {
   return gulp.src(Paths.SCSS_TOOLKIT_SOURCES)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write(Paths.HERE))
-    .pipe(gulp.dest(Paths.CSS));
+      .pipe(sourcemaps.init())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer())
+      .pipe(sourcemaps.write(Paths.HERE))
+      .pipe(gulp.dest(Paths.CSS));
 });
 
-gulp.task('watch', function() {
+// Task to copy HTML files to dist
+gulp.task('copy-html', function () {
+  return gulp.src(Paths.HTML)
+      .pipe(gulp.dest(Paths.DIST));
+});
+
+// Task to copy images to dist
+gulp.task('copy-images', function () {
+  return gulp.src(Paths.IMAGES)
+      .pipe(gulp.dest(path.join(Paths.DIST, 'assets/img')));
+});
+
+// Clean the dist folder (use dynamic import)
+gulp.task('clean', function () {
+  return import('del').then(del => {
+    return del.deleteSync([Paths.DIST]);
+  });
+});
+
+// Watch task for changes
+gulp.task('watch', function () {
   gulp.watch(Paths.SCSS, gulp.series('compile-scss'));
 });
 
-gulp.task('open', function() {
-  gulp.src('pages/about-us.html')
-    .pipe(open());
+// Open the app
+gulp.task('open', function () {
+  gulp.src('index.html')
+      .pipe(open());
 });
 
+// Open app with watching
 gulp.task('open-app', gulp.parallel('open', 'watch'));
+
+// Build task: Clean dist, compile SCSS, and copy HTML/images
+gulp.task('build', gulp.series('clean', 'compile-scss', 'copy-html', 'copy-images'));
